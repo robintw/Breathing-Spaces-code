0e845748546934b32baefb5beedcf6987beb6959
Robin Wilson <robin@rtwilson.com>
2019-09-02 11:11:23 +0100

    Add library code to get AQ data using the azure functions and put into sensible format. Also funcs for loading Flo's data to same format.

---
+++ get_aq_data.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from joblib import Memory
+from azure_table_interface import query_aq_data
+
+# Set up caching for the Azure table access
+memory = Memory('./_cache_')
+
+ID_to_name = {'nesta-1': 'Priory Rd (South)',
+              'nesta-2': 'Priory Rd (North)',
+              'nesta-2-1': 'Priory Rd (North)',
+              'nesta-4': 'Horseshoe Bridge',
+              'nesta-5': 'Kent Rd',
+              'nesta-6': 'Portswood Rd',
+              'nesta-7': 'St Denys Rd',
+              'nesta-8': 'Priory Rd-Kent Rd junction'}
+
+@memory.cache
+def get_sensor_data(sensor_id, **kwargs):
+    res = query_aq_data(sensor_id,
+                        **kwargs)
+    
+    res = res.resample('15min').mean()
+    
+    if len(res.columns) == 1:
+        res.columns = [sensor_id.split("_")[1]]
+    
+    return res
+
+def get_all_sensor_data(col='pm25'):
+    sensor_ids = ['aq-deployment_nesta-' + s for s in ['1', '2', '2-1', '4', '5', '6', '7', '8']]
+    
+    dfs = [get_sensor_data(sensor_id, from_date='2019-01-01', cols=[col]) for sensor_id in sensor_ids]
+    
+    data = pd.concat(dfs, axis=1)
+    
+    # Deal with nesta-2 and nesta-2-1 sensors which are in the same location, but -2-1 was a replacement
+    # after -2 failed. Just combine the two columns as one
+    data['nesta-2'] = data['nesta-2'].combine_first(data['nesta-2-1'])
+    del data['nesta-2-1']
+    
+    hourly_mean = data.resample('1H').mean()
+    daily_mean = data.resample('1D').mean()
+    
+    return data, hourly_mean, daily_mean
+
+def read_flo_csv(col='pm25'):
+    col_to_select = col + "_mean"
+
+    flo_data = pd.read_csv('../Data/BS Sensors/20190307 to 20190823_15min averages_StDenys_6sensors.csv',
+                           parse_dates=[2], dayfirst=True)
+    flo_data = flo_data.drop('Unnamed: 0', axis=1)
+    flo_data = flo_data[['site', 'date', col_to_select]]
+
+    flo_data = flo_data.pivot(columns='site', index='date', values='pm25_mean')
+
+    flo_data.columns = [col.lower() for col in flo_data.columns]
+    flo_data.index.name = 'timestamp'
+
+    flo_data['nesta-2'] = flo_data['nesta-2'].combine_first(flo_data['nesta-2-1'])
+    del flo_data['nesta-2-1']
+
+    hourly_mean = flo_data.resample('1H').mean()
+    daily_mean = flo_data.resample('1D').mean()
+    
+    return flo_data, hourly_mean, daily_mean
